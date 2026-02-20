b30c1eef9cf29c3ad5ead80cc94a4e3d1dea0503
winchoose <tmdxor741@naver.com>
2026-02-20 21:02:45 +0900

    feat: 에세스토큰, 리프레시 토큰 자동발급

--- src/features/auth/login-callback.tsx
+++ src/features/auth/login-callback.tsx
@@ -7,18 +7,19 @@ export default function LoginCallback() {
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
-
     const token = params.get('token');
 
-    if (!token) {
+    const finalToken = token ?? localStorage.getItem('accessToken');
+
+    if (!finalToken) {
       navigate(routePath.ONBOARDING, { replace: true });
       return;
     }
 
-    // 🔐 토큰 저장
-    localStorage.setItem('accessToken', token);
+    if (token) {
+      localStorage.setItem('accessToken', token);
+    }
 
-    // 메인으로 이동
     navigate(routePath.MAIN, { replace: true });
   }, [navigate]);
 
--- src/shared/api/config/instance.ts
+++ src/shared/api/config/instance.ts
@@ -1,58 +1,15 @@
 import ky from 'ky';
-import { routePath } from '@app/router/path';
+import {
+  handleCheckAndSetToken,
+  handleUnauthorizedResponse,
+} from './interceptor';
 
 export const api = ky.create({
   prefixUrl: import.meta.env.VITE_BASE_URL,
+  credentials: 'include', // 쿠키
   retry: 0,
-
   hooks: {
-    // 🔥 요청 전에 accessToken 자동 주입
-    beforeRequest: [
-      (request) => {
-        const token = localStorage.getItem('accessToken');
-
-        if (token) {
-          request.headers.set('Authorization', `Bearer ${token}`);
-        }
-      },
-    ],
-
-    afterResponse: [
-      async (request, options, response) => {
-        if (response.status !== 401) return;
-
-        const refreshToken = localStorage.getItem('refreshToken');
-
-        if (!refreshToken) {
-          localStorage.clear();
-          window.location.href = routePath.ONBOARDING;
-          return;
-        }
-
-        try {
-          const refreshResponse = await ky
-            .post(`${import.meta.env.VITE_BASE_URL}/token/refresh`, {
-              headers: {
-                'Refresh-Token': refreshToken,
-              },
-            })
-            .json<{ accessToken: string }>();
-
-          localStorage.setItem('accessToken', refreshResponse.accessToken);
-
-          // 🔥 기존 요청 헤더 교체
-          request.headers.set(
-            'Authorization',
-            `Bearer ${refreshResponse.accessToken}`,
-          );
-
-          // 🔥 원래 요청 다시 실행
-          return ky(request);
-        } catch {
-          localStorage.clear();
-          window.location.href = routePath.ONBOARDING;
-        }
-      },
-    ],
+    beforeRequest: [handleCheckAndSetToken],
+    afterResponse: [handleUnauthorizedResponse],
   },
 });
